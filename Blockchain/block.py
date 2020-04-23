e5edc10d45cd6ffa56855d3952a3bb7f5a00ead7
1mb1r <alexei.titovsky@gmail.com>
2020-04-23 14:50:47 +0300

    update

--- Blockchain/block.py
+++ Blockchain/block.py
@@ -5,11 +5,25 @@ from time import time
 
 # path to chain dir
 chain_dir = os.curdir + '/chain/'
-
+files = os.listdir(chain_dir)
+files = sorted(int(i) for i in files)
 
 def get_hash(file_name):          # hash def \__0__/
 	file = open(chain_dir + file_name, 'rb').read()# указываем имя файла, который мы открываем для чтения с указанием папки
 	return hashlib.sha256(file).hexdigest()
+
+def check_integrity():
+    for file in files[1:]:
+        h = json.load(open(chain_dir + str(file)))['hash']
+        actual_hash = get_hash(str(file - 1))
+
+        prev_block = str(file -1)
+
+        if h == actual_hash:
+            res = 'kk'
+        else:
+            res = 'corrupted'
+        print('block {} is {}'.format(prev_block, res))
 
 
 def get_files():
@@ -40,6 +54,7 @@ def main():
 	message = input()
 	recipient = input()
 	write_block(sender, amount, message, recipient)
+	check_integrity()
 
 
 if __name__ == '__main__':
