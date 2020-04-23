83fc4b6126d41a5faa3279085f0b05f16e49667f
1mb1r <alexei.titovsky@gmail.com>
2020-04-23 16:45:15 +0300

    account+transaction update

--- Blockchain/block.py
+++ Blockchain/block.py
@@ -1,6 +1,8 @@
 import json
 import os
 import hashlib
+import random
+import string
 from time import time
 
 # path to chain dir
@@ -25,30 +27,52 @@ def check_integrity():
             res = 'corrupted'
         print('block {} is {}'.format(prev_block, res))
 
+def create_account(wallet):
+	userid = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(16))
+	user = {'index': int(str(files[-1] + 1)),
+			'timestamp': time(),
+			'userid': userid,
+			'wallet': wallet
+	}
+	with open(chain_dir + str(files[-1] + 1), 'w') as file:
+		json.dump(user, file, indent=4, ensure_ascii=False)
+
 
 def write_block(sender, amount, message, recipient, prev_hash=''):
 	prev_file = files[-1]# последний блок, который находится в папке chain
 	file_name = str(prev_file + 1)# называем след.файл символьным значением
 	prev_hash = get_hash(str(prev_file))# олучаем хэш предыдущего файла
 
-	block = {'index': int(file_name),
+	for file in files[1:]:
+		u = json.load(open(chain_dir + str(file)))['userid']
+		if u == sender:
+			sw = json.load(open(chain_dir + str(file)))['wallet']
+			sender_wallet = sw - amount
+
+	for file in files[1:]:
+		u = json.load(open(chain_dir + str(file)))['userid']
+		if u == recipient:
+			rw = json.load(open(chain_dir + str(file)))['wallet']
+			recipient_wallet = rw + amount
+
+	block = {'index': int(str(files[-1] + 1)),
 			'timestamp': time() ,
 			 'sender' : sender,
 			 'amount' : amount,
 			 'message' : message,
 			 'recipient' : recipient,
+			 'sender_wallet': sender_wallet,
+			 'recipient_wallet': recipient_wallet,
 			 'hash': prev_hash}
 
 	with open(chain_dir + file_name, 'w') as file:
-		json.dump(block, file)
+		json.dump(block, file, indent=4, ensure_ascii=False)
 
 def main():
-	sender = input()
-	amount = int(input())
-	message = input()
-	recipient = input()
-	write_block(sender, amount, message, recipient)
-	check_integrity()
+
+	#write_block(input(), int(input()), input(), input())
+	# check_integrity()
+	#create_account(int(input()))
 
 
 if __name__ == '__main__':
