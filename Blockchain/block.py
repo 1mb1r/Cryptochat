76448a3e106d142af7c8f4f6c4a0586d5d188030
1mb1r <alexei.titovsky@gmail.com>
2020-06-19 17:15:13 +0300

    upload txt

--- Blockchain/block.py
+++ Blockchain/block.py
@@ -38,41 +38,36 @@ def create_account(wallet):
 		json.dump(user, file, indent=4, ensure_ascii=False)
 
 
-def write_block(sender, amount, message, recipient, prev_hash=''):
+def write_block( amount, prev_hash=''):
 	prev_file = files[-1]  # последний блок, который находится в папке chain
 	file_name = str(prev_file + 1)  # называем след.файл символьным значением
 	prev_hash = get_hash(str(prev_file))  # олучаем хэш предыдущего файла
+	transactions = True
+	proof = True
+	sender = True
+	recipient = True
+	index = True
 
-	for file in files[1:]:
-		u = json.load(open(chain_dir + str(file)))['userid']
-		if u == sender:
-			sw = json.load(open(chain_dir + str(file)))['wallet']
-			sender_wallet = sw - amount
-
-	for file in files[1:]:
-		u = json.load(open(chain_dir + str(file)))['userid']
-		if u == recipient:
-			rw = json.load(open(chain_dir + str(file)))['wallet']
-			recipient_wallet = rw + amount
-
-	block = {'index': int(str(files[-1] + 1)),
-			'timestamp': time() ,
-			 'sender' : sender,
-			 'amount' : amount,
-			 'message' : message,
-			 'recipient' : recipient,
-			 'sender_wallet': sender_wallet,
-			 'recipient_wallet': recipient_wallet,
-			 'hash': prev_hash}
+	block = { 'index': index,
+    		  'timestamp': time(),
+   			  'transactions': [
+        	{
+              'sender': sender,
+              'recipient': recipient,
+              'amount': amount,
+        	}
+   		],
+             'proof': proof,
+             'previous_hash': prev_hash}
 
 	with open(chain_dir + file_name, 'w') as file:
 		json.dump(block, file, indent=4, ensure_ascii=False)
 
 def main():
 
-	#write_block(input(), int(input()), input(), input())
+	write_block(input())
 	# check_integrity()
-	create_account(int(input()))
+	#create_account(int(input()))
 
 
 if __name__ == '__main__':
